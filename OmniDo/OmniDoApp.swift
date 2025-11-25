700d29fa51417fcbe07a44fbf09d6f46d807e675
Tuziking <bobo1398861921@gmail.com>
2025-11-25 18:58:13 +0800

    Initial Commit

---
+++ OmniDo/OmniDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmniDoApp.swift
+//  OmniDo
+//
+//  Created by BOBO on 2025/11/25.
+//
+
+import SwiftUI
+
+@main
+struct OmniDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
